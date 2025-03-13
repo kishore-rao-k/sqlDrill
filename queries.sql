f79d3bf9fd53031b1034130dcc9f793055cc4248
kishore-rao-k <kishoreraok12@gmail.com>
2025-03-13 19:31:42 +0530

    Initial commit

---
+++ queries.sql
@@ -0,0 +1,30 @@
+-- create database chat_store;
+use chat_store;
+-- create table organization( id int auto_increment primary key, name text);
+-- create table channel(id int auto_increment primary key, name text);
+-- create table user(id int auto_increment primary key, name text);
+-- create table message(id int auto_increment primary key,post_time datetime, content text);
+show tables;
+-- alter table channel
+-- add column organization_id INT;
+
+-- alter table channel
+-- add constraint fk_organization
+-- foreign key(organization_id) references organization(id);
+
+-- insert into organization(name)
+-- values ("Lambda School");
+
+-- insert into channel(name, organization_id)
+-- values('#general',1),('#random',1);
+
+-- insert into user(name)
+-- values("Alice"),("Bob"),("Chris");
+
+-- select * from organization;
+-- select * from channel;
+ -- select * from user;
+
+
+
+
